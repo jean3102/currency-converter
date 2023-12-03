daa0c551539327aa41fdda5aeba3ea14ea362815
jean carlos <jeancarlos3102@gmail.com>
2023-12-03 12:45:58 -0500

    delete console.log

--- src/services/getCurrencies .ts
+++ src/services/getCurrencies .ts
@@ -1,6 +1,5 @@
 export const getCurrencies = async () => {
 	const apiKey = import.meta.env.VITE_API_KEY;
-	console.log(`🚀 ------------ apiKey:`, apiKey)
 	const apiUrl = `https://api.apilayer.com/fixer/symbols`;
 
 	const response = await fetch(apiUrl, {
